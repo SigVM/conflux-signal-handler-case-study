e647dd7d0782c0cc128aafeb92ea590e0eff3a58
zandent <simon.zhao@mail.utoronto.ca>
2020-08-08 23:52:43 -0400

    add js test for osm and passed. TODO: make a formal osm

---
+++ makerdao/js_osm/deployOSM.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+
+const { Conflux } = require('js-conflux-sdk');
+
+const PRIVATE_KEY_OSM = '0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef';
+
+async function main() {
+  const cfx = new Conflux({
+    url: 'http://localhost:12539',
+    defaultGasPrice: 1,
+    defaultGas: 1000000,
+    logger: console,
+  });
+
+  console.log(cfx.defaultGasPrice); // 100
+  console.log(cfx.defaultGas); // 1000000
+
+  // ================================ Account =================================
+  const accountosm = cfx.Account(PRIVATE_KEY_OSM); // create account instance
+  console.log(accountosm.address);
+
+  // ================================ Contract ================================
+  // create contract instance
+  const contractosm = cfx.Contract({
+    abi: require('./contract/OSM-abi.json'),
+    bytecode: require('./contract/OSM-bytecode.json'),
+  });
+  // create contract instance
+  const contractds_val = cfx.Contract({
+    abi: require('./contract/DSValue-abi.json'),
+    // code is unnecessary
+    address: '0x8df4fade46019b611baed0c03875055942255aee',
+  });
+  // deploy the contract, and get `contractCreated`
+  const receiptosm = await contractosm.constructor(contractds_val.address)
+    .sendTransaction({from: accountosm,
+                      gas: 10000000 })
+    .confirmed();
+  console.log(receiptosm);
+}
+
+main().catch(e => console.error(e));
---
+++ makerdao/js_osm/deploydsval.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+
+const { Conflux } = require('js-conflux-sdk');
+
+const PRIVATE_KEY_OSM = '0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef';
+
+async function main() {
+  const cfx = new Conflux({
+    url: 'http://localhost:12539',
+    defaultGasPrice: 1,
+    defaultGas: 1000000,
+    logger: console,
+  });
+
+  console.log(cfx.defaultGasPrice); // 100
+  console.log(cfx.defaultGas); // 1000000
+
+  // ================================ Account =================================
+  const accountosm = cfx.Account(PRIVATE_KEY_OSM); // create account instance
+  console.log(accountosm.address);
+
+  // ================================ Contract ================================
+  // create contract instance
+  const contractdaval = cfx.Contract({
+    abi: require('./contract/DSValue-abi.json'),
+    bytecode: require('./contract/DSValue-bytecode.json'),
+  });
+
+  // deploy the contract, and get `contractCreated`
+  const receiptds = await contractdaval.constructor()
+    .sendTransaction({ from: accountosm })
+    .confirmed();
+  console.log(receiptds);
+}
+
+main().catch(e => console.error(e));
